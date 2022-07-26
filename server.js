dc059a87db521215bf6735fe5890b9c78a10eeda
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-07-25 22:43:06 -0400

    add middleware to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// global variables 
 const express = require('express');
 const app = express ();
 const fs = require('fs');
@@ -5,3 +6,9 @@ const path = require('path');
 const notes = require('./db/db.json');
 const randomId = require ('helper/uuid.js')
 const PORT = process.env.PORT || 3001;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
